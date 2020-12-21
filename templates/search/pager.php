b8e906168e0b15e7565cac062cc1bbec86194ac0
Omaya Noureddine <orn@administrate.co>
2020-12-21 14:02:54 +0400

    PLAT-8413 - Integrate full pager

--- templates/search/pager.php
+++ templates/search/pager.php
@@ -1,52 +1,71 @@
 <?php
 global $wp;
 $queryVars = array();
-$pagedParam = '?paged=';
+
 if (get_query_var('query')) {
     $queryVars['query'] = get_query_var('query');
-    $pagedParam = '&paged=';
 }
 if (get_query_var('lcat')) { 
     $queryVars['lcat'] = get_query_var('lcat');
-    $pagedParam = '&paged=';
 }
 if (get_query_var('paged')) {
     $queryVars['paged'] = get_query_var('paged');
 }
-$currentUrl = add_query_arg( $queryVars, home_url( $wp->request ) . '/');
 
 $currentPageNumber = $searchResults['currentPage'];
 $totalPageNumber = $searchResults['totalNumPages'];
 
-if (isset($_GET['paged']) && !empty($_GET['paged'])) {
-    $pattern = '/paged=' . $currentPageNumber . '/';
-    $firstPageUrl = preg_replace($pattern, 'paged=1', $currentUrl);
-    $lastPageUrl = preg_replace($pattern, 'paged=' . $totalPageNumber, $currentUrl);
-    $prevPageUrl = preg_replace($pattern, 'paged=' . ($currentPageNumber - 1), $currentUrl);
-    $nextPageUrl = preg_replace($pattern, 'paged=' . ($currentPageNumber + 1), $currentUrl);
-} else {
-    $firstPageUrl = $currentUrl . $pagedParam . 1;
-    $lastPageUrl = $currentUrl . $pagedParam . $totalPageNumber;
-    $prevPageUrl = $currentUrl . $pagedParam . ($currentPageNumber - 1);
-    $nextPageUrl = $currentUrl . $pagedParam . ($currentPageNumber + 1);
-}
+$httpBuildQuery = http_build_query($queryVars);
+$currentUrl = home_url( $wp->request ) . '/?' . $httpBuildQuery;
+
+$queryVars['paged'] = 1;
+$httpBuildQuery = http_build_query($queryVars);
+$firstPageUrl = home_url( $wp->request ) . '/?' . $httpBuildQuery;
+
+$queryVars['paged'] = $totalPageNumber;
+$httpBuildQuery = http_build_query($queryVars);
+$lastPageUrl = home_url( $wp->request ) . '/?' . $httpBuildQuery;
+
+$queryVars['paged'] = $currentPageNumber - 1;
+$httpBuildQuery = http_build_query($queryVars);
+$prevPageUrl = home_url( $wp->request ) . '/?' . $httpBuildQuery;
+
+$queryVars['paged'] = $currentPageNumber + 1;
+$httpBuildQuery = http_build_query($queryVars);
+$nextPageUrl = home_url( $wp->request ) . '/?' . $httpBuildQuery;
+
 $prevPage = '';
 $nextPage = '';
+$pagination = array(
+    'first' => '',
+    'prev' => '',
+    'current' => '<span>' . $currentPageNumber . '/' . $totalPageNumber . '</span>',
+    'next' => '',
+    'last' => ''
+);
 if ($searchResults['hasPreviousPage'] == true) :
-    $prevPage = '<a href="' . $prevPageUrl . '" class="admwpp-prev">' . __('prev <', ADMWPP_TEXT_DOMAIN) . '</a>';
+    $pagination['first'] = '<a href="' . $firstPageUrl . '" class="admwpp-first first">' . __('<< first', ADMWPP_TEXT_DOMAIN) . '</a>';
+    $pagination['prev']  = '<a href="' . $prevPageUrl . '" class="admwpp-prev">' . __('< prev', ADMWPP_TEXT_DOMAIN) . '</a>';
 endif;
 if ($searchResults['hasNextPage'] == true) :
-    $nextPage = '<a href="' . $nextPageUrl . '" class="admwpp-next">' . __('> next', ADMWPP_TEXT_DOMAIN) . '</a>';
+    $pagination['next'] = '<a href="' . $nextPageUrl . '" class="admwpp-next">' . __('next >', ADMWPP_TEXT_DOMAIN) . '</a>';
+    $pagination['last'] = '<a href="' . $lastPageUrl . '" class="admwpp-last last">' . __('last >>', ADMWPP_TEXT_DOMAIN) . '</a>';
 endif;
 
 if ($searchResults['totalNumPages'] > 1) :
     if ($pager == 'simple') : ?>
         <div class="admwpp-pager admwpp-simple-pager">
-            <a href="<?php echo esc_url($firstPageUrl); ?>" class='admwpp-first first'>first <<</a>
-            <?php echo $prevPage; ?>
-            <span><?php echo $currentPageNumber . '/' . $totalPageNumber; ?></span>
-            <?php echo $nextPage; ?>
-            <a href="<?php echo esc_url($lastPageUrl); ?>" class='admwpp-last last'>>> last</a>
+            <?php echo implode("", $pagination); ?>
         </div>
-    <?php endif;
+    <?php else : ?>
+        <div class="admwpp-pager admwpp-full-pager">
+            <?php echo paginate_links(array(
+                'format' => '?paged=%#%',
+                'total' => $totalPageNumber,
+                'prev_text' => '<i class="fa fa-chevron-left"></i>',
+                'next_text' => '<i class="fa fa-chevron-right"></i>',
+                'end_size' => 3,
+            )); ?>
+       </div>
+<?php endif;
 endif; ?>
