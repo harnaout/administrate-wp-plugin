f5210588cd5ea7040be0d192f089242296d8cc9b
Jad Khater <jck@administrate.co>
2021-04-08 15:30:27 +0300

    PLAT-10057 Add minimum places custom filter template

---
+++ templates/search/minplaces-filter.php
@@ -0,0 +1,4 @@
+<div class="admwpp-minplaces-filter">
+    <label for="from"><?php _e("Minimum Places Remaining", 'admwpp'); ?></label>
+    <input type="number" class="admwpp-number" id="admwpp-minplaces" name="minplaces" value="<?php echo $minplaces; ?>" autocomplete="off">
+</div>
